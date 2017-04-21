2f5a8ec6e737b6107cf4c6de057ef4c86fb30c95
Groups <esokullu@gmail.com>
2017-04-21 23:57:18 +0300

    Added documentation for edge files.

---
+++ src/Pho/Framework/Exceptions/InvalidParticleMethodException.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Pho package.
+ *
+ * (c) Emre Sokullu <emre@phonetworks.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pho\Framework\Exceptions;
+
+use Pho\Framework\ParticleInterface;
+
+/**
+ * Thrown when a particle is called with an unknown function
+ * (e.g invalid edge etc.) 
+ * 
+ * Since all particle functions are dynamic (using magic method
+ * _call) erroneous method calls are possible and not handled 
+ * by native PHP errors.
+ * 
+ * @author Emre Sokullu <emre@phonetworks.org>
+ */
+class InvalidParticleMethodException extends \Exception {
+
+    /**
+     * Constructor.
+     *
+     * @param string $class_name Class name of the particle
+     * @param string $method Invalid method
+     */
+    public function __construct(string $class_name, string $method)
+    {
+        parent::__construct();
+        $this->message = sprintf(
+            "The particle %s was called with the invalid method: %s",
+            $class_name,
+            $method
+        );
+    }
+
+}
