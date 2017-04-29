34d80ac55281984fa8acfb86900a4396f68fab16
Groups <esokullu@gmail.com>
2017-04-30 00:06:10 +0300

    relocated tests

--- tests/Pho/Framework/SimpleTest.php
+++ tests/Pho/Framework/SimpleTest.php
@@ -9,9 +9,8 @@
  * file that was distributed with this source code.
  */
 
-// namespace Pho\Framework\Tests;
+namespace Pho\Framework;
 
-use Pho\Framework;
 use Pho\Lib\Graph;
 
 class SimpleTest extends \PHPUnit\Framework\TestCase 
@@ -27,39 +26,39 @@ class SimpleTest extends \PHPUnit\Framework\TestCase
     }
 
     public function testActor() {
-        $node = new Framework\Actor($this->graph);
+        $node = new Actor($this->graph);
         $node_expected_to_be_identical = $this->graph->get($node->id());
         $this->assertEquals($node->id(), $node_expected_to_be_identical->id());
     }
 
     public function testActorEdge() {
-        $actor = new Framework\Actor($this->graph);
-        $object = new Framework\Object($actor, $this->graph);
+        $actor = new Actor($this->graph);
+        $object = new Object($actor, $this->graph);
         $edge = $actor->writes($object);
-        $this->assertInstanceOf(Framework\ActorOut\Writes::class, $edge);
+        $this->assertInstanceOf(ActorOut\Writes::class, $edge);
         $this->assertInstanceOf(Graph\Predicate::class, $edge->predicate());
     }
 
     public function testActorPredicate() {
-        $actor = new Framework\Actor($this->graph);
-        $object = new Framework\Object($actor, $this->graph);
+        $actor = new Actor($this->graph);
+        $object = new Object($actor, $this->graph);
         $edge = $actor->subscribes($object);
-        $this->assertInstanceOf(Framework\ActorOut\SubscribesPredicate::class, $edge->predicate());
+        $this->assertInstanceOf(ActorOut\SubscribesPredicate::class, $edge->predicate());
     }
 
     public function testObjectGetter() {
-        $actor = new Framework\Actor($this->graph);
-        $object = new Framework\Object($actor, $this->graph);
+        $actor = new Actor($this->graph);
+        $object = new Object($actor, $this->graph);
         $edge = $actor->writes($object);
-        $this->assertInstanceOf(Framework\ActorOut\Writes::class, $object->getWriters()[0]);
+        $this->assertInstanceOf(ActorOut\Writes::class, $object->getWriters()[0]);
         $this->assertCount(1, $object->getWriters());
         $this->assertCount(1, $actor->getWrites());
-        $this->assertInstanceOf(Framework\ActorOut\Writes::class, $actor->getWrites()[0]);
+        $this->assertInstanceOf(ActorOut\Writes::class, $actor->getWrites()[0]);
     }
 
      public function testFiltering() {
-        $actor = new Framework\Actor($this->graph);
-        $object = new Framework\Object($actor, $this->graph);
+        $actor = new Actor($this->graph);
+        $object = new Object($actor, $this->graph);
         $edge = $actor->writes($object);
         $edge = $actor->reads($object);
         $this->assertCount(1, $actor->getWrites());
@@ -69,8 +68,8 @@ class SimpleTest extends \PHPUnit\Framework\TestCase
      * Since write extends subscribes
      */
     public function testEdgeInheritance() {
-        $actor = new Framework\Actor($this->graph);
-        $object = new Framework\Object($actor, $this->graph);
+        $actor = new Actor($this->graph);
+        $object = new Object($actor, $this->graph);
         $edge = $actor->writes($object);
         $this->assertCount(1, $actor->getSubscriptions());
         $this->assertCount(0, $actor->getReads());
@@ -80,24 +79,24 @@ class SimpleTest extends \PHPUnit\Framework\TestCase
      * @expectedException     Pho\Framework\Exceptions\InvalidEdgeHeadTypeException
      */
     public function testImpossibleEdge() {
-        $actor1 = new Framework\Actor($this->graph);
-        $actor2 = new Framework\Actor($this->graph);
+        $actor1 = new Actor($this->graph);
+        $actor2 = new Actor($this->graph);
         $edge = $actor1->writes($actor2);
     }
 
     public function testEdgeInvoke() {
-        $actor = new Framework\Actor($this->graph);
-        $object = new Framework\Object($actor, $this->graph);
+        $actor = new Actor($this->graph);
+        $object = new Object($actor, $this->graph);
         $edge = $actor->writes($object);
-        $this->assertInstanceOf(Framework\Object::class, $edge());
+        $this->assertInstanceOf(Object::class, $edge());
         $this->assertEquals($object->id(), $edge()->id());
     }
 
 
     public function testActorToArray() {
-        $actor = new Framework\Actor($this->graph);
+        $actor = new Actor($this->graph);
         $array = $actor->toArray();
-        $faker = Faker\Factory::create();
+        $faker = \Faker\Factory::create();
         $this->assertArrayHasKey("id", $array);
         $this->assertArrayHasKey("attributes", $array);
         $this->assertCount(0, $array["attributes"]);
@@ -111,9 +110,9 @@ class SimpleTest extends \PHPUnit\Framework\TestCase
     }
 
     public function testFrameToArray() {
-        $faker = Faker\Factory::create();
-        $actor = new Framework\Actor($this->graph);
-        $frame = new Framework\Frame($actor, $this->graph);
+        $faker = \Faker\Factory::create();
+        $actor = new Actor($this->graph);
+        $frame = new Frame($actor, $this->graph);
         $edge = $actor->writes($frame);
         $array = $frame->toArray();
         $this->assertArrayHasKey("id", $array);
