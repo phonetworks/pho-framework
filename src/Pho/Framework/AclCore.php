42da107ef7f9631384162df888b3ce3d75428701
Emre Sokullu <emre@groups-inc.com>
2017-05-01 14:53:05 +0300

    added documentation for AclCore

--- src/Pho/Framework/AclCore.php
+++ src/Pho/Framework/AclCore.php
@@ -4,16 +4,51 @@ namespace Pho\Framework;
 
 use Pho\Lib\Graph;
 
+/**
+ * AclCore (Access Control Lists Core)
+ * 
+ * Access Control List is a gateway authority between graph nodes.
+ * Pho Framework introduces the core of this authority as an 
+ * abstract implementation and expects the higher level packages
+ * to extend itself. The core is by default immutable; meaning
+ * the two values (creator and context) cannot be altered.
+ * 
+ * @author Emre Sokullu <emre@phonetworks.org>
+ */
 class AclCore {
 
+    /**
+     * Who created this node. Must point to an Actor.
+     *
+     * @var Actor
+     */
     protected $creator;
+
+    /**
+     * In what context this node was created. Must point to a node 
+     * that implements Pho\Lib\Graph\GraphInterface
+     * @var Pho\Lib\Graph\GraphInterface
+     */
     protected $context;
 
+    /**
+     * Constructor.
+     * 
+     * @param Actor $creator The creator of this node.
+     * @param \Pho\Lib\Graph\GraphInterface $context The context in which this node is created and will exist
+     */
     public function __construct(Actor $creator, Graph\GraphInterface $context) {
         $this->creator = $creator;
         $this->context = $context;
     }
 
+    /**
+     * Converts the object into a portable PHP array
+     *
+     * Useful for custom serialization.
+     * 
+     * @return array
+     */
     public function toArray(): array
     {
         //eval(\Psy\sh());
