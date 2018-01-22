314d8a754f04342d035fa1cd052dd8eade922b25
Groups <esokullu@gmail.com>
2018-01-21 22:04:28 -0800

    passing tests

--- src/Pho/Framework/Handlers/Has.php
+++ src/Pho/Framework/Handlers/Has.php
@@ -56,11 +56,17 @@ class Has implements HandlerInterface
         $name = lcfirst(substr($name, 3)); // we don't camelize, because we want the call to be made in proper format.
 
 
-        if(static::methodExists($pack, $name, $id, Direction::out())) {
+        if(static::edgeMethodExists($pack, $name, $id, Direction::out())) {
             return static::checkEdgeNode($particle, $pack, $name, $id, Direction::out());
         }   
-        elseif(static::methodExists($pack, $name, $id, Direction::in())) {
+        elseif(static::edgeMethodExists($pack, $name, $id, Direction::in())) {
             return static::checkEdgeNode($particle, $pack, $name, $id, Direction::in());
+        }
+        elseif(static::edgeCallableExists($pack, $name, $id, Direction::out())) {
+            return static::checkEdgeItself($particle, $pack, $name, $id, Direction::out());
+        }   
+        elseif(static::edgeCallableExists($pack, $name, $id, Direction::in())) {
+            return static::checkEdgeItself($particle, $pack, $name, $id, Direction::in());
         }
 
         throw new \Pho\Framework\Exceptions\InvalidParticleMethodException(__CLASS__, $original_name);
@@ -75,7 +81,7 @@ class Has implements HandlerInterface
      * 
      * @return bool
      */
-    protected static function methodExists(
+    protected static function edgeMethodExists(
         array $pack,
         string $name,
         ID $id,
@@ -87,7 +93,29 @@ class Has implements HandlerInterface
 
 
     /**
-     * "Has" Catcher for Edges Out
+     * Whether the given method is available for incoming or outgoing edge callables.
+     *
+     * @param array $pack Holds incoming and outgoing cargos.
+     * @param string $name Method name. Queried among incoming and outgoing labels.
+     * @param Direction $direction Direction in question; in or out.
+     * 
+     * @return bool
+     */
+    protected static function edgeCallableExists(
+        array $pack,
+        string $name,
+        ID $id,
+        Direction $direction 
+        ): bool
+    {
+        return !is_null($pack[(string) $direction]->callable_edge_singularLabels) && in_array($name, $pack[(string) $direction]->callable_edge_singularLabels);
+    }
+
+
+    /**
+     * "Has" Catcher for Edges 
+     * 
+     * @todo Check if it performs better with $edge->$node_adj()->equals($id) is first or last
      *
      * @param ParticleInterface $particle The particle that this handler is associated with.
      * @param array  $pack Holds cargo variables extracted by loaders.
@@ -113,6 +141,41 @@ class Has implements HandlerInterface
             if(
                 $edge instanceof $cargo->singularLabel_class_pairs[$name] 
                 && $edge->$node_adj()->equals($id)
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * "Has" Catcher for Edge Callables
+     * 
+     * @todo Check if it performs better with $edge->id()->equals($id) is first or last
+     *
+     * @param ParticleInterface $particle The particle that this handler is associated with.
+     * @param array  $pack Holds cargo variables extracted by loaders.
+     * @param string $name The edge node label in plural.
+     * @param ID $id The ID of the node in question.
+     * @param Direction  $direction The direction of the edge adjacent nodes to look up.
+     * 
+     * @return bool whether the node exists or not
+     */
+    protected static function checkEdgeItself(
+        ParticleInterface $particle, 
+        array $pack,
+        string $name,
+        ID $id,
+        Direction $direction 
+        ): bool
+    {
+        $direction = (string) $direction;
+        $cargo = $pack[$direction];
+        $edges = $particle->edges()->$direction();
+        foreach($edges as $edge) {
+            if(
+                $edge->id()->equals($id) &&
+                $edge instanceof $cargo->callable_edge_singularLabel_class_pairs[$name]
             ) {
                 return true;
             }
---
+++ tests/Pho/Framework/CallableTest.php
@@ -0,0 +1,113 @@
+<?php
+
+/*
+ * This file is part of the Pho package.
+ *
+ * (c) Emre Sokullu <emre@phonetworks.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Pho\Framework;
+
+use Pho\Lib\Graph\ID;
+use Pho\Lib\Graph\Node;
+
+/**
+ * An edge example, with simple callable names.
+ */
+class Comment extends AbstractEdge // Actor -> Object (Comment)
+{
+    const TAIL_CALLABLE_LABEL = "comment";
+    const TAIL_CALLABLE_LABELS = "comments";
+    const HEAD_CALLABLE_LABEL = "comment";
+    const HEAD_CALLABLE_LABELS = "comments";
+}
+
+class CommentPredicate extends Predicate {}
+
+/**
+ * A complicated edge example, with callable names in
+ * camelized format.
+ */
+class Message extends AbstractEdge // Actor -> Actor (Message)
+{
+    const TAIL_CALLABLE_LABEL = "outgoingMessage";
+    const TAIL_CALLABLE_LABELS = "outgoingMessages";
+    const HEAD_CALLABLE_LABEL = "incomingMessage";
+    const HEAD_CALLABLE_LABELS = "incomingMessages";
+}
+
+class MessagePredicate extends Predicate {}
+
+
+class CallableTest extends \PHPUnit\Framework\TestCase 
+{
+    private $space, $actor, $random_id;
+
+    public function setUp() {
+        $this->space = new Space();
+        $this->actor = new Actor($this->space);
+        $this->random_id = ID::generate(new Node($this->space)); // can never collide, different entity types.
+    }
+
+    public function tearDown() {
+        unset($this->space);
+        unset($this->actor);
+    }
+
+    public function testSimpleCallable() {
+        $obj = new Object($this->actor, $this->space);
+        $comment = new Comment($this->actor, $obj);
+        $this->actor->registerOutgoingEdges(Comment::class);
+        $obj->registerIncomingEdges(Comment::class);
+        $this->assertCount(1, $obj->getComments());
+        $this->assertCount(1, $this->actor->getComments());
+        $this->assertTrue($obj->hasComment($comment->id()));
+        $this->assertTrue($this->actor->hasComment($comment->id()));
+        $this->assertFalse($this->actor->hasComment($this->random_id));
+        $this->assertFalse($obj->hasComment($this->random_id));
+    }
+
+    public function testComplicatedCallable() {
+        $actor = new Actor($this->space);
+        
+        $this->actor->registerOutgoingEdges(Message::class);
+        $this->actor->registerIncomingEdges(Message::class);
+        $actor->registerOutgoingEdges(Message::class);
+        $actor->registerIncomingEdges(Message::class);
+
+        $message = new Message($this->actor, $actor);
+        //eval(\Psy\sh());
+        $this->assertCount(1, $actor->getIncomingMessages());
+        $this->assertCount(0, $actor->getOutgoingMessages());
+        $this->assertCount(0, $this->actor->getIncomingMessages());
+        $this->assertCount(1, $this->actor->getOutgoingMessages());
+        $this->assertTrue($actor->hasIncomingMessage($message->id()));
+        $this->assertTrue($this->actor->hasOutgoingMessage($message->id()));
+        $this->assertFalse($this->actor->hasIncomingMessage($message->id()));
+        $this->assertFalse($actor->hasOutgoingMessage($message->id()));
+
+        $message = new Message($this->actor, $actor);
+        $this->assertCount(2, $actor->getIncomingMessages());
+        $this->assertCount(0, $actor->getOutgoingMessages());
+        $this->assertCount(0, $this->actor->getIncomingMessages());
+        $this->assertCount(2, $this->actor->getOutgoingMessages());
+        $this->assertTrue($actor->hasIncomingMessage($message->id()));
+        $this->assertTrue($this->actor->hasOutgoingMessage($message->id()));
+        $this->assertFalse($this->actor->hasIncomingMessage($message->id()));
+        $this->assertFalse($actor->hasOutgoingMessage($message->id()));
+
+        $message = new Message($actor, $this->actor);
+        $this->assertCount(2, $actor->getIncomingMessages());
+        $this->assertCount(1, $actor->getOutgoingMessages());
+        $this->assertCount(1, $this->actor->getIncomingMessages());
+        $this->assertCount(2, $this->actor->getOutgoingMessages());
+        $this->assertFalse($actor->hasIncomingMessage($message->id()));
+        $this->assertFalse($this->actor->hasOutgoingMessage($message->id()));
+        $this->assertTrue($this->actor->hasIncomingMessage($message->id()));
+        $this->assertTrue($actor->hasOutgoingMessage($message->id()));
+    }
+    
+}
